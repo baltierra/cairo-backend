b8aaa7d71e2c8f3b6cafc78b360cf9d64a165850
baltierra <fabs@illinois.edu>
2025-12-05 12:06:39 -0600

    Fix feedback email settings

--- config/settings.py
+++ config/settings.py
@@ -147,10 +147,17 @@ EMAIL_BACKEND = env(
 
 DEFAULT_FROM_EMAIL = env(
     "DEFAULT_FROM_EMAIL",
-    default="no-reply@historicalcairo.com",
+    default="support@historicalcairo.com",
 )
 
 FEEDBACK_RECIPIENT = env(
     "FEEDBACK_RECIPIENT",
     default="support@historicalcairo.com",
 )
+
+EMAIL_HOST = env("EMAIL_HOST", default="localhost")
+EMAIL_PORT = env.int("EMAIL_PORT", default=25)
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=False)
+EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=False)
